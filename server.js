550d9a90b08806734f29734f9f1ab61a6321fafc
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-26 17:48:16 +0530

    feat: Added API as per requirement

--- server.js
+++ server.js
@@ -2,40 +2,59 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./server-keys.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
-function sendNotification(registrationToken) {
-const message = {
-  data: {
-    score: '850',
-    time: '2:45'
-  },
-  token: registrationToken
-};
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Send a message to the device corresponding to the provided
-// registration token.
-admin.messaging().send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
-  });
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+async function sendNotification(registrationToken, day = "yesterday") {
+    try {
+        const message = {
+            data: {
+                title: "FCM Push Notification",
+                subtitle: "Check out action buttons",
+                day
+            },
+            token: registrationToken,
+            android: {
+                priority: "high",
+            },
+        }
+
+        const res = await admin.messaging().send(message)
+        return res;
+    } catch (err) {
+        console.log(err.errorInfo)
+        throw new Error(JSON.stringify(err.errorInfo))
+    }
 }
 
-
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.post('/', (req, res) => {
-  sendNotification("dULpAoVIR_q77q8gPKtuxV:APA91bES42msZqUkYQ00fwsh_rKNmYor2D-iv1Tm5QbCbyLGywKilapexV0YSQpOP_1-CxDpO5h8lbcE9VLejjZOTivA5NK5IhD5NU_7aDFu1gixp1Xe7Jj-ncUU0BMZkselT597EI5A")
-  res.send('Hello World!')
-})
+app.post('/', async (req, res) => {
+    try {
+        const {
+            token,
+            day
+        } = req.body;
+        const data = await sendNotification(token, day)
+        res.json({
+            success: true,
+            data
+        });
+    } catch (err) {
+        console.error(err)
+        res.json({
+            success: false,
+            err
+        })
+    }
+});
 
 app.listen(port, () => {
-  console.log(`Firebase backend app listening on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
